ccfdd39f7e8fec7c40a5f30e005bc0a1bddbf734
Hussnain Raza <hussnain444all@gmail.com>
2024-06-09 16:21:23 +0500

    fixed timeout decorator

--- index.ts
+++ index.ts
@@ -5,34 +5,18 @@
  * @param {number} milliseconds - The time limit in milliseconds.
  * @returns {MethodDecorator} The method decorator.
  */
-export function Timeout(milliseconds: number): MethodDecorator {
+function Timeout(milliseconds: number): MethodDecorator {
     return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor): PropertyDescriptor {
         const originalMethod = descriptor.value;
 
-        descriptor.value = function (...args: any[]): any {
-            if (originalMethod.constructor.name === 'AsyncFunction') {
-                // If the original method is async, return a promise
-                return new Promise(async (resolve, reject) => {
-                    try {
-                        const result = await Promise.race([
-                            originalMethod.apply(this, args),
-                            new Promise((_, reject) => setTimeout(() => reject(new Error(`${String(propertyKey)} timed out`)), milliseconds))
-                        ]);
-                        resolve(result);
-                    } catch (error) {
-                        reject(error);
-                    }
-                });
-            } else {
-                // If the original method is synchronous, execute it directly
-                try {
-                    return originalMethod.apply(this, args);
-                } catch (error) {
-                    throw error;
-                }
-            }
+        descriptor.value = async function (...args: any[]): Promise<any> {
+            return Promise.race([
+                originalMethod.apply(this, args),
+                new Promise((_, reject) => setTimeout(() => reject(new Error(`${String(propertyKey)} timed out`)), milliseconds))
+            ]);
         };
 
         return descriptor;
     };
 }
+
