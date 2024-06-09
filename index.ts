e51b9470617d1768e063df693303450cbd595586
Hussnain Raza <hussnain444all@gmail.com>
2024-06-09 12:49:02 +0500

    updated package

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-function Timeout(milliseconds: number): MethodDecorator {
+/**
+ * A decorator that enforces a time limit on the execution of an async method.
+ * If the method does not complete within the specified time limit, it will be rejected.
+ *
+ * @param {number} milliseconds - The time limit in milliseconds.
+ * @returns {MethodDecorator} The method decorator.
+ */
+export function Timeout(milliseconds: number): MethodDecorator {
     return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor): PropertyDescriptor {
         const originalMethod = descriptor.value;
 
